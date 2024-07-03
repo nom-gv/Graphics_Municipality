aad74ee15d1fc8e408c33688837c92ca2ba93d2a
nom-gv <noemi.guarachi@gmail.com>
2024-07-03 10:19:42 -0400

    Add More Config

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ app.layout = html.Div([
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [8, 5, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montreal'},
             ],
             'layout': {
